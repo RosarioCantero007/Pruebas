431e1461ed4abfd766de7cc2609c1947039d12b9
Rosario Cantero <rcantero@elreydeltornillo.com>
2023-06-09 11:13:26 -0600

    Archivo php prueba con texto y tabla

--- index_prueba.php
+++ index_prueba.php
@@ -1,3 +1,31 @@
 <?php
     echo 'Hola  mundo 3';
 ?>
+
+<h1>Texto de prueba</h1>
+<p>recomendable pero no indispensable 4 horas de ayuno. Tiempo de entrega: 1 día hábil De lunes a sábado, los resultados que se toman antes de las 10:00 horas estarán disponibles el mismo día a partir de las 18:00 horas. Los resultados de las muestras que se toman después de las 10:00 horas estarán disponibles el día siguiente a partir de las 14:00 horas. Los resultados de las muestras que se reciban en el laboratorio el día domingo, estarán disponibles el día siguiente a partir de las 14:00.</p>
+
+<table class="default">
+
+  <tr>
+
+    <td>Celda 1</td>
+
+    <td>Celda 2</td>
+
+    <td>Celda 3</td>
+
+  </tr>
+
+  <tr>
+
+    <td>Celda 4</td>
+
+    <td>Celda 5</td>
+
+    <td>Celda 6</td>
+
+  </tr>
+
+</table>
+
